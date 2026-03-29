374ee96b4cc097907d68f57e2c8cd996171e426b
Hermane Boavida <hermaneboavida@gmail.com>
2026-03-29 00:54:18 +0000

    File changes

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -5,26 +5,26 @@ import { cva } from "class-variance-authority";
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-bold transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md font-bold transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
   {
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground font-bold shadow hover:bg-primary/90",
+          "bg-primary text-black font-bold shadow hover:bg-primary/90",
         destructive:
-          "bg-destructive text-white font-bold shadow-sm hover:bg-destructive/90",
+          "bg-destructive text-black font-bold shadow-sm hover:bg-destructive/90",
         outline:
-          "border border-input bg-background font-bold shadow-sm hover:bg-accent hover:text-accent-foreground",
+          "border border-input bg-background text-black font-bold shadow-sm hover:bg-accent hover:text-black",
         secondary:
-          "bg-secondary text-secondary-foreground font-bold shadow-sm hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground font-bold",
-        link: "text-primary underline-offset-4 hover:underline font-bold",
+          "bg-secondary text-black font-bold shadow-sm hover:bg-secondary/80",
+        ghost: "hover:bg-accent text-black font-bold hover:text-black",
+        link: "text-black underline-offset-4 hover:underline font-bold",
       },
       size: {
-        default: "h-9 px-4 py-2",
-        sm: "h-8 rounded-md px-3 text-xs",
-        lg: "h-10 rounded-md px-8",
-        icon: "h-9 w-9",
+        default: "h-9 px-4 py-2 text-base",
+        sm: "h-8 rounded-md px-3 text-base",
+        lg: "h-10 rounded-md px-8 text-base",
+        icon: "h-9 w-9 text-base",
       },
     },
     defaultVariants: {
